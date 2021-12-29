e753e428989300f1a8425705a97152bbee8e3e7a
Huuhuy93 <huynguyen20101993@gmail.com>
2021-12-29 14:56:52 +0700

    ok

---
+++ mudule_2/src/ss1_introduce_to_java/thuc_hanh/SystemTime.java
@@ -0,0 +1,8 @@
+package ss1_introduce_to_java.thuc_hanh;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println();
+    }
+}
