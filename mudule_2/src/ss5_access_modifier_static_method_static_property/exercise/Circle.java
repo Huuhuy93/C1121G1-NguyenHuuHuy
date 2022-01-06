b56e218ecfceded840c611ad13f08b1501d6d904
Huuhuy93 <huynguyen20101993@gmail.com>
2022-01-06 13:49:42 +0700

    up bai 3

---
+++ mudule_2/src/ss5_access_modifier_static_method_static_property/exercise/Circle.java
@@ -0,0 +1,20 @@
+package ss5_access_modifier_static_method_static_property.exercise;
+
+public class Circle {
+    final double PI = 3.14;
+    private double radius = 1.0;
+    private String color = "red";
+    Circle() {
+
+    }
+    Circle(double r) {
+        radius = r;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+    public double getArea() {
+        return 
+    }
+}
