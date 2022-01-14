f59bd0574cbbd4234dcc88e7f7446de529469a4d
Huuhuy93 <huynguyen20101993@gmail.com>
2022-01-14 16:49:50 +0700

    up bai 11

--- mudule_2/src/ss11_java_collection_framework/exercise/product_use_arraylist/model/service/MobilephoneServiceImpl.java
+++ mudule_2/src/ss11_java_collection_framework/exercise/product_use_arraylist/model/service/MobilephoneServiceImpl.java
@@ -6,14 +6,13 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MobilephoneServiceImpl implements IServiceProduct {
-    static ArrayList<MobilePhone> mobilePhonesArrayList = new ArrayList<>();
+    static ArrayList<MobilePhone> mobilePhonesList = new ArrayList<>();
+
     static {
-        mobilePhonesArrayList.add(new MobilePhone(1,"mobilephone",5000,"13 pro max","apple"));
-        mobilePhonesArrayList.add(new MobilePhone(2,"mobilephone",3000,"note 20","samsung"));
-        mobilePhonesArrayList.add(new MobilePhone(3,"mobilephone",2000,"z3","sony"));
+        mobilePhonesList.add(new MobilePhone(1, "mobilephone", 5000, "13 pro max", "apple"));
+        mobilePhonesList.add(new MobilePhone(2, "mobilephone", 3000, "note 20", "samsung"));
+        mobilePhonesList.add(new MobilePhone(3, "mobilephone", 2000, "z3", "sony"));
     }
-
-
 
 
     @Override
@@ -30,13 +29,13 @@ public class MobilephoneServiceImpl implements IServiceProduct {
         System.out.println("nhập hãng sản xuất");
         String brand = sc.nextLine();
         MobilePhone mobilePhone = new MobilePhone(id, nameProduct, price, name, brand);
-        mobilePhonesArrayList.add(mobilePhone);
+        mobilePhonesList.add(mobilePhone);
         System.err.println("Thêm mới thành công");
     }
 
     @Override
     public void display() {
-        for (MobilePhone temp: mobilePhonesArrayList) {
+        for (MobilePhone temp : mobilePhonesList) {
             if (temp == null) {
                 break;
             } else {
@@ -50,20 +49,41 @@ public class MobilephoneServiceImpl implements IServiceProduct {
         Scanner sc = new Scanner(System.in);
         System.out.println("Nhập id cần xóa");
         int idDlelete = Integer.parseInt(sc.nextLine());
-        for (int i = 0; i < mobilePhonesArrayList.size(); i++ ) {
-            if (mobilePhonesArrayList.get(i).getId() == idDlelete) {
-                mobilePhonesArrayList.remove(i);
+        for (int i = 0; i < mobilePhonesList.size(); i++) {
+            if (mobilePhonesList.get(i).getId() == idDlelete) {
+                mobilePhonesList.remove(i);
                 System.out.println("Đã xóa thành công");
+            } else {
+                System.out.println("không có phần tử cần xóa");
             }
-         else{
-                    System.out.println("không có phần tử cần xóa");
-                }
-            }
+        }
     }
 
     @Override
     public void edit() {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập id bạn cần chỉnh sửa");
+        int idEdit = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < mobilePhonesList.size(); i++) {
+            if (i == idEdit) {
+                System.out.println("nhập id cần chỉnh sửa");
+                int id = Integer.parseInt(sc.nextLine());
+                System.out.println("nhập tên sản phẩm cần chỉnh sửa");
+                String nameProduct = sc.nextLine();
+                System.out.println("nhập giá của sản phẩm cần chỉnh sửa");
+                int price = Integer.parseInt(sc.nextLine());
+                System.out.println("nhập loại sản phẩm cần chỉnh sửa ");
+                String name = sc.nextLine();
+                System.out.println("nhập hãng sản xuất cần chỉnh sửa");
+                String brand = sc.nextLine();
+                mobilePhonesList.get(i).setId(id);
+                mobilePhonesList.get(i).setNameProduct(nameProduct);
+                mobilePhonesList.get(i).setPriceProduct(price);
+                mobilePhonesList.get(i).setNamePhone(name);
+                mobilePhonesList.get(i).setBrand(brand);
+                break;
+            }
+        }
     }
 
     @Override
@@ -71,7 +91,7 @@ public class MobilephoneServiceImpl implements IServiceProduct {
         Scanner sc = new Scanner(System.in);
         System.out.println("Nhập tên sản phẩm cần tìm kiếm");
         String nameSearch = sc.nextLine();
-        for (MobilePhone mobilePhone: mobilePhonesArrayList) {
+        for (MobilePhone mobilePhone : mobilePhonesList) {
             if (mobilePhone.getNamePhone().equalsIgnoreCase(nameSearch)) {
                 System.out.println(mobilePhone);
             }
