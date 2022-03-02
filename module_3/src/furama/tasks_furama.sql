eb95e305e3a1312f30c7684fd00cd57815d79d69
Huuhuy93 <huynguyen20101993@gmail.com>
2022-03-02 22:12:28 +0700

    up bai 4

--- module_3/src/furama/tasks_furama.sql
+++ module_3/src/furama/tasks_furama.sql
@@ -62,8 +62,126 @@ INSERT INTO `case_study_furama`.`dv_di_kem` VALUES ('4', 'Buffet buổi sáng', 
 INSERT INTO `case_study_furama`.`dv_di_kem` VALUES ('5', 'Buffet buổi trưa', '90000', 'suất', 'đầy đủ đồ ăn, tráng miệng');
 INSERT INTO `case_study_furama`.`dv_di_kem` VALUES ('6', 'Buffet buổi tối', '16000', 'suất', 'đầy đủ đồ ăn, tráng miệng');
 
+INSERT INTO `case_study_furama`.`hop_dong`  VALUES ('1', '2020-12-08', '2020-12-08', '0', '3', '1', '3');
+INSERT INTO `case_study_furama`.`hop_dong`  VALUES ('2', '2020-07-14', '2020-07-21', '200000', '7', '3', '1');
+INSERT INTO `case_study_furama`.`hop_dong`  VALUES ('3', '2021-03-15', '2021-03-17', '50000', '3', '4', '2');
+INSERT INTO `case_study_furama`.`hop_dong`  VALUES ('4', '2021-01-14', '2021-01-18', '100000', '7', '5', '5');
+INSERT INTO `case_study_furama`.`hop_dong`  VALUES ('5', '2021-07-14', '2021-07-15', '0', '7', '2', '6');
+INSERT INTO `case_study_furama`.`hop_dong`  VALUES ('6', '2021-06-01', '2021-06-03', '0', '7', '7', '6');
+INSERT INTO `case_study_furama`.`hop_dong`  VALUES ('7', '2021-09-02', '2021-09-05', '100000', '7', '4', '4');
+INSERT INTO `case_study_furama`.`hop_dong`  VALUES ('8', '2021-06-17', '2021-06-18', '150000', '3', '4', '1');
+INSERT INTO `case_study_furama`.`hop_dong`  VALUES ('9', '2020-11-19', '2020-11-19', '0', '3', '4', '3');
+INSERT INTO `case_study_furama`.`hop_dong`  VALUES ('10', '2021-04-12', '2021-04-14', '0', '10', '3', '5');
+INSERT INTO `case_study_furama`.`hop_dong`  VALUES ('11', '2021-04-25', '2021-04-25', '0', '2', '2', '1');
+INSERT INTO `case_study_furama`.`hop_dong`  VALUES ('12', '2021-05-25', '2021-05-27', '0', '7', '10', '1');
+
+INSERT INTO `case_study_furama`.`hop_dong_chi_tiet`  VALUES ('1', '2', '4', '5');
+INSERT INTO `case_study_furama`.`hop_dong_chi_tiet`  VALUES ('2', '2', '5', '8');
+INSERT INTO `case_study_furama`.`hop_dong_chi_tiet`  VALUES ('3', '2', '6', '15');
+INSERT INTO `case_study_furama`.`hop_dong_chi_tiet`  VALUES ('4', '3', '1', '1');
+INSERT INTO `case_study_furama`.`hop_dong_chi_tiet`  VALUES ('5', '3', '2', '11');
+INSERT INTO `case_study_furama`.`hop_dong_chi_tiet`  VALUES ('6', '1', '3', '1');
+INSERT INTO `case_study_furama`.`hop_dong_chi_tiet`  VALUES ('7', '1', '2', '2');
+INSERT INTO `case_study_furama`.`hop_dong_chi_tiet`  VALUES ('8', '12', '2', '2');
+
+
+
 -- task 2 : Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là một trong
 -- các ký tự “H”, “T” hoặc “K” và có tối đa 15 kí tự.
+select * from nhan_vien where ho_ten like 'H%' or ho_ten like 'T%' or ho_ten like 'K%' and char_length(ho_ten) <= 15 ;
+
+-- task 3 : Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi và có địa chỉ ở “Đà Nẵng” hoặc “Quảng Trị”.
+select * from khach_hang
+    where (year(curdate()) - year(ngay_sinh)) - (right(curdate(), 5) < right(ngay_sinh, 5)) between 18 and 50
+    and dia_chi like '%Đà Nẵng'
+    or dia_chi like '%Quảng Trị';
+    
+-- task 4 : Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu lần. Kết quả hiển thị được sắp xếp tăng dần
+--  theo số lần đặt phòng của khách hàng. Chỉ đếm những khách hàng nào có Tên loại khách hàng là “Diamond”.
+select khach_hang.ma_kh, khach_hang.ho_ten, count(hop_dong.ma_hop_dong) as so_luong from hop_dong
+join khach_hang on hop_dong.ma_kh = khach_hang.ma_kh
+where khach_hang.ma_loai_khach = 1 
+group by khach_hang.ma_kh order by so_luong;
+
+-- task 5 : Hiển thị ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc, tong_tien
+--  (Với tổng tiền được tính theo công thức như sau: Chi Phí Thuê + Số Lượng * Giá, với Số Lượng và Giá là từ bảng dich_vu_di_kem,
+--  hop_dong_chi_tiet) cho tất cả các khách hàng đã từng đặt phòng. (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
+select khach_hang.ma_kh, khach_hang.ho_ten, loai_khach.ten_loai_khach, hop_dong.ma_hop_dong, hop_dong.ngay_lam_hop_dong, hop_dong.ngay_ket_thuc,
+ dich_vu.ten_dv, sum(ifnull(dich_vu.chi_phi_thue, 0) + (ifnull(hop_dong_chi_tiet.so_luong, 0) * ifnull(dv_di_kem.gia, 0))) as tong_tien from khach_hang
+ left join loai_khach on khach_hang.ma_loai_khach = loai_khach.ma_loai_khach
+ left join hop_dong on khach_hang.ma_kh = hop_dong.ma_kh
+ left join dich_vu on hop_dong.ma_dv = dich_vu.ma_dv
+ left join hop_dong_chi_tiet on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+ left join dv_di_kem on hop_dong_chi_tiet.ma_dv_di_kem = dv_di_kem.ma_dv_di_kem
+ group by hop_dong.ma_hop_dong;
+
+  -- task 6 : Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ chưa từng được
+--  khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3)002E
+select dich_vu.ma_dv, dich_vu.ten_dv, dich_vu.dien_tich, dich_vu.chi_phi_thue, loai_dv.ten_loai_dv from dich_vu
+join loai_dv on dich_vu.ma_loai_dv = loai_dv.ma_loai_dv
+join hop_dong on dich_vu.ma_dv = hop_dong.ma_dv
+where not hop_dong.ngay_lam_hop_dong between '2021-01-01 00:00:00' and '2021-03-31 23:59:59' 
+group by hop_dong.ma_dv
+order by dich_vu.chi_phi_thue desc;
+
+-- task 7 : Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu của tất cả các loại
+--  dịch vụ đã từng được khách hàng đặt phòng trong năm 2020 nhưng chưa từng được khách hàng đặt phòng trong năm 2021.
+select dich_vu.ma_dv, dich_vu.ten_dv, dich_vu.dien_tich, dich_vu.chi_phi_thue, loai_dv.ten_loai_dv from dich_vu
+join loai_dv on dich_vu.ma_loai_dv = loai_dv.ma_loai_dv
+join hop_dong on dich_vu.ma_dv = hop_dong.ma_dv
+where hop_dong.ngay_lam_hop_dong between '2020-01-01 00:00:00' and '2020-12-31 23:59:59'
+and not hop_dong.ngay_lam_hop_dong between '2021-01-01 00:00:00' and '2021-12-31 23:59:59'
+group by hop_dong.ma_dv;
+
+-- task 8 : Hiển thị thông tin ho_ten khách hàng có trong hệ thống, với yêu cầu ho_ten không trùng nhau.
+select distinct ho_ten from khach_hang;
+
+
+
+-- task 10 : Hiển thị thông tin tương ứng với từng hợp đồng thì đã sử dụng bao nhiêu dịch vụ đi kèm. Kết quả hiển thị bao gồm ma_hop_dong,
+--  ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc, so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem).
+select hop_dong.ma_hop_dong, hop_dong.ngay_lam_hop_dong, hop_dong.ngay_ket_thuc, hop_dong.tien_dat_coc, sum(ifnull(hop_dong_chi_tiet.so_luong, 0))
+as so_luong_dich_vu_di_kem
+from hop_dong
+left join hop_dong_chi_tiet on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+left join dv_di_kem on hop_dong_chi_tiet.ma_dv_di_kem = dv_di_kem.ma_dv_di_kem
+group by hop_dong.ma_hop_dong;
+
+-- task 11 : Hiển thị thông tin các dịch vụ đi kèm đã được sử dụng bởi những khách hàng có ten_loai_khach là “Diamond” và có dia_chi
+--  ở “Vinh” hoặc “Quảng Ngãi”.
+select dv_di_kem.ma_dv_di_kem, dv_di_kem.ten_dv_di_kem from dv_di_kem
+join hop_dong_chi_tiet on dv_di_kem.ma_dv_di_kem = hop_dong_chi_tiet.ma_dv_di_kem
+join hop_dong on hop_dong_chi_tiet.ma_hop_dong = hop_dong.ma_hop_dong
+join khach_hang on hop_dong.ma_kh = khach_hang.ma_kh
+join loai_khach on khach_hang.ma_loai_khach = loai_khach.ma_loai_khach
+where loai_khach.ten_loai_khach like '%Diamond' and khach_hang.dia_chi like '%Vinh' or khach_hang.dia_chi like '%Quảng Ngãi';
+
+-- task 12 : Hiển thị thông tin ma_hop_dong, ho_ten (nhân viên), ho_ten (khách hàng), so_dien_thoai (khách hàng),
+--  ten_dich_vu, so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem), tien_dat_coc của tất cả các
+--  dịch vụ đã từng được khách hàng đặt vào 3 tháng cuối năm 2020 nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2021.
+select hop_dong.ma_hop_dong, nhan_vien.ho_ten as ten_nhan_vien, khach_hang.ho_ten as ten_khach_hang, khach_hang.so_dt, dich_vu.ma_dv,
+ dich_vu.ten_dv, sum(ifnull(hop_dong_chi_tiet.so_luong, 0)) as so_luong_dich_vu_di_kem, hop_dong.tien_dat_coc from dich_vu
+join hop_dong on dich_vu.ma_dv = hop_dong.ma_dv
+join hop_dong_chi_tiet on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+join nhan_vien on hop_dong.ma_nv = nhan_vien.ma_nv
+join khach_hang on hop_dong.ma_kh = khach_hang.ma_kh
+where hop_dong.ngay_lam_hop_dong between '2020-10-01 00:00:00' and '2020-12-31 23:59:59'
+and not hop_dong.ngay_lam_hop_dong between '2021-01-01 00:00:00' and '2020-06-30 23:59:59'
+group by hop_dong.ma_hop_dong;
+
+-- task 13 : Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng.
+--  (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
+select
+
+
+
+
+
+
+
+
+
+
 
 
 
